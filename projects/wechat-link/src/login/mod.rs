2806f1a4a62284d20cca04a4fa8b1ea4a5dbcb40
Aster <galaster@foxmail.com>
2024-08-01 18:31:13 +0800

    Check if login is prohibited

--- projects/wechat-link/src/login/mod.rs
+++ projects/wechat-link/src/login/mod.rs
@@ -1,8 +1,10 @@
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::Write;
-use std::time::{SystemTime, UNIX_EPOCH};
-use reqwest::Client;
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
+use reqwest::{Client, Error};
+use serde::{Deserialize, Serialize};
+use tokio::time::sleep;
 
 pub async fn get_uuid() -> Result<String, reqwest::Error> {
     let client = Client::new();
@@ -40,4 +42,111 @@ pub async fn get_qrcode(uuid: &str) -> Result<(), reqwest::Error> {
     file.write_all(&bytes).unwrap();
 
     Ok(())
+}
+
+
+async fn call_login_status(uuid: &str) -> Result<LoginStatus, reqwest::Error> {
+    let client = Client::new();
+    let mut params = HashMap::new();
+    params.insert("tip", "1");
+    params.insert("uuid", uuid);
+    let time = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs().to_string();
+    params.insert("_", &time);
+    params.insert("loginicon", "true");
+
+    let response = client.get("https://login.wx.qq.com/cgi-bin/mmwebwx-bin/login")
+        .query(&params)
+        .send()
+        .await?;
+
+    let body = response.text().await?;
+    println!("S: {body}");
+    if body.starts_with("window.code=201") {
+        Ok(LoginStatus::Waiting)
+    } else if body.contains("window.code=400") {
+        Ok(LoginStatus::Timeout)
+    } else if body.contains("window.code=200") {
+        let text = body.rsplit("\"").nth(1).unwrap();
+        Ok(LoginStatus::Success {
+            redirect_uri: text.to_string()
+        })
+    } else {
+        Ok(LoginStatus::Waiting)
+    }
+}
+
+#[derive(Clone, Debug)]
+pub enum LoginStatus {
+    Waiting,
+    Scanned,
+    Success {
+        redirect_uri: String,
+    },
+    Timeout,
+}
+
+
+pub async fn poll_login_status(uuid: &str) -> Result<String, reqwest::Error> {
+    let mut elapsed_time = Duration::from_secs(0);
+    let max_wait_time = Duration::from_secs(60);
+
+    loop {
+        match call_login_status(uuid).await {
+            Ok(LoginStatus::Waiting) => {
+                println!("LoginStatus::Waiting")
+            }
+            Ok(LoginStatus::Scanned) => {
+                println!("LoginStatus::Scanned")
+            }
+            Ok(LoginStatus::Timeout) => {
+                println!("Maximum wait time of 10 seconds reached.");
+                return Err(panic!());
+            }
+            Ok(LoginStatus::Success { redirect_uri }) => {
+                return Ok(redirect_uri)
+            }
+            Err(_) => {}
+        }
+
+        sleep(Duration::from_millis(200)).await;
+        elapsed_time += Duration::from_millis(200);
+
+        if elapsed_time >= max_wait_time {
+            println!("Maximum wait time of 10 seconds reached.");
+            return Err(panic!());
+        }
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct LoginInfo {
+    pub ret: i32,
+    pub message: String,
+    pub skey: String,
+    pub wxsid: String,
+    pub wxuin: String,
+    pub pass_ticket: String,
+    pub isgrayscale: i32,
+}
+
+pub async fn get_login_info(redirect_uri: &str) -> Result<String, reqwest::Error> {
+    let client = Client::new();
+    let mut params = HashMap::new();
+    params.insert("fun", "new");
+    params.insert("version", "v2");
+    let time = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs().to_string();
+    params.insert("_", &time);
+
+    let response = client.get(redirect_uri)
+        .query(&params)
+        .send()
+        .await?;
+
+    let login_info = response.text().await?;
+
+    if login_info.contains("暂不支持") {
+        panic!("禁用")
+    }
+
+    Ok(login_info)
 }
--- projects/wechat-test/src/main.rs
+++ projects/wechat-test/src/main.rs
@@ -1,19 +1,24 @@
 #![deny(missing_debug_implementations, missing_copy_implementations)]
 #![warn(missing_docs, rustdoc::missing_crate_level_docs)]
 #![doc = include_str!("../readme.md")]
-#![doc(html_logo_url = "https://raw.githubusercontent.com/oovm/shape-rs/dev/projects/images/Trapezohedron.svg")]
-#![doc(html_favicon_url = "https://raw.githubusercontent.com/oovm/shape-rs/dev/projects/images/Trapezohedron.svg")]
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/oovm/shape-rs/dev/projects/images/Trapezohedron.svg"
+)]
+#![doc(
+    html_favicon_url = "https://raw.githubusercontent.com/oovm/shape-rs/dev/projects/images/Trapezohedron.svg"
+)]
 
-use wechat_link::login::{get_qrcode, get_uuid};
+use wechat_link::login::{get_login_info, get_qrcode, get_uuid, poll_login_status};
 
 #[tokio::main]
 async fn main() {
     match get_uuid().await {
         Ok(uuid) => {
             get_qrcode(&uuid).await.unwrap();
-
-
-        },
+            let link = poll_login_status(&uuid).await.unwrap();
+            let info = get_login_info(&link).await.unwrap();
+            println!("{info:#?}")
+        }
         Err(err) => eprintln!("Error: {}", err),
     }
 }
